1bdff77eb90b11985c3c9fd8048dc26c9eed3059
Neto Russo <netoacrn1@gmail.com>
2024-06-14 03:08:58 -0300

    subindo o trabalho final V0.1

---
+++ scripts/menu.js
@@ -0,0 +1,35 @@
+const abrir = document.getElementById("menuConteudo");
+const fechar = document.getElementById("btnFecharModalMenu");
+const fundo = document.getElementById("fundoModal");
+const conteudo = document.getElementById("menuMobileConteudo");
+const modal = document.getElementById("modal");
+let estadoDoModal = false;
+
+function abrirEFechar() {
+  if (estadoDoModal) {
+
+    conteudo.classList.remove("aberto");
+    conteudo.classList.add("fechado");
+    setTimeout(() => {
+      modal.style = "display: none;";
+      estadoDoModal = false;
+    }, 500)
+
+
+  } else {
+    modal.style = "display: fixed;";
+    conteudo.classList.remove("fechado");
+    conteudo.classList.add("aberto");
+    estadoDoModal = true;
+  }
+}
+
+abrir.addEventListener("click", (e) => {
+  e.preventDefault();
+  abrirEFechar()
+});
+
+fechar.addEventListener("click", (e) => {
+  e.preventDefault();
+  abrirEFechar()
+});
